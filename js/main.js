946fab2ee2c89d4fe659348f229a3ea337a53af0
Kirill Khomenko <kkhomenko10@gmail.com>
2022-12-03 14:20:50 +0100

    fix code following the consultation issues

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 // Для рекурсивної функції піднесення числа до степеня pow(base, exponent) реалізувати валідацію значень, що передаються, і генерацію помилок відповідних типів.
 // Виклик функції вкласти в блок try з відловом виняткових ситуацій (помилок) різних типів з оповіщенням користувача про тип помилки.
 
-const myPow = function (base, exponent) {
-  return exponent === 1 ? base : base * myPow(base, exponent - 1);
+const recPow = function (base, exponent) {
+  return exponent === 0 ? 1 : base * recPow(base, exponent - 1);
 };
 
-function validatePower(value) {
-  if (typeof value !== 'number') {
-    throw new TypeError('Value should be a number.');
-  } else if (value <= 0 || !Number.isSafeInteger(value)) {
+function myPow(base, value) {
+  if (typeof value !== 'number' || typeof base !== 'number') {
+    throw new TypeError('All meanings should be a number.');
+  } else if (
+    value <= 0 ||
+    !Number.isSafeInteger(value) ||
+    !Number.isSafeInteger(base)
+  ) {
     throw new RangeError(
-      'Value must be greater than 0 and it should be an integer value.'
+      'Value must be greater than 0 and all meanings should be an integer value.'
     );
   } else {
-    return value;
+    return recPow(base, value);
   }
 }
 
 try {
-  const validatedPower = validatePower(3);
-  console.log('Result = ' + myPow(2, validatedPower));
+  const validatedPower = myPow(3, 7);
+  console.log('Result = ' + validatedPower);
 } catch (error) {
   if (error instanceof TypeError) {
     console.log('error : ', error);
